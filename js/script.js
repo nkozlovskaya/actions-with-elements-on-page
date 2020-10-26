1e7d48fe9de430c504aad3798aeee1f0dffbd291
Nady <catsnake@yandex.ru>
2020-10-26 15:47:43 +0300

    actions on page with element

---
+++ js/script.js
@@ -0,0 +1,49 @@
+"use strict";
+
+const box = document.getElementById("box"),
+  btns = document.getElementsByTagName("button"),
+  circles = document.getElementsByClassName("circle"),
+  hearts = document.querySelectorAll(".heart"),
+  oneHeart = document.querySelector(".heart"),
+  wrapper = document.querySelector(".wrapper");
+
+// hearts.forEach((item) => {
+//   console.log(item);
+// });
+
+// box.style.backgroundColor = "blue";
+// box.style.width = "500px";
+box.style.cssText = "background-color: blue; width: 500px";
+
+btns[1].style.borderRadius = "100%";
+circles[0].style.backgroundColor = "red";
+
+// for (let i = 0; i < hearts.length; i++) {
+//   hearts[i].style.backgroundColor = "green";
+// }
+hearts.forEach((item) => {
+  item.style.backgroundColor = "green";
+});
+
+const div = document.createElement("div");
+
+// const text = document.createTextNode("тут был я");
+
+div.classList.add("black");
+
+// document.body.append(div);
+
+// wrapper.append(div);
+
+wrapper.prepend(div);
+// hearts[0].before(div);
+hearts[0].after(div);
+
+// circles[0].remove();
+
+// hearts[1].replaceWith(div);
+
+div.innerHTML = "<h1>Hello World</h1>";
+// div.textContent='Hello';
+
+div.insertAdjacentHTML("beforebegin", "<h2>Hello</h2>");
